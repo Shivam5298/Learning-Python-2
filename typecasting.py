08c10be0757d7da248b086aa1afd2436d9b48f14
Shivam5298 <sganguly2096@gmail.com>
2026-04-29 20:00:42 +0530

    Version 1.0

---
+++ typecasting.py
@@ -0,0 +1,23 @@
+a = 34
+print(a)
+print(type(a))
+
+b = "34"
+print(b)
+print(type(b))
+
+# convert b into an Integer
+
+c = int(b)
+print(c)
+print(type(c))
+
+# We can also convert number to string
+
+d = 223
+print(d)
+print(type(d))
+
+e = str(d)
+print(e)
+print(type(e))
---
+++ user_input.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter another Number: "))
+
+sum = num1 + num2
+
+print("Sum of entered Numbers are: ",sum)
